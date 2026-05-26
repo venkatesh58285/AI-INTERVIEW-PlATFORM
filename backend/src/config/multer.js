5ac3144710097f0a5cae37b56d90889fe47b1203
venkatesh58285 <venkyrachamadugu169@gmail.com>
2026-05-26 20:46:10 +0530

    multer done

---
+++ backend/src/config/multer.js
@@ -0,0 +1,28 @@
+import multer from "multer";
+
+// STORAGE CONFIG
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+// FILE FILTER
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "application/pdf") {
+    cb(null, true);
+  } else {
+    cb(new Error("Only PDF files allowed"), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+});
+
+export default upload;
